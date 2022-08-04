5abdef7b91b7b21e2aab927e3606714625b94d85
Lawson <lawsonmcphetridge2020@gmail.com>
2022-08-04 16:16:49 -0700

    added goblins with object to screen

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 // import functions and grab DOM elements
-
+const goblinEl = document.getElementById('goblin-section');
 // let state
+const goblins = [
+    {
+        name: 'lawson',
+        hp: 5,
+    },
+    {
+        name: 'ryan',
+        hp: 6,
+    },
+];
+
+for (let goblin of goblins) {
+    const goblinContainer = document.createElement('div');
+    goblinContainer.textContent = `I am ${goblin.name} 👺 and i have ${goblin.hp} health left`;
+    goblinContainer.classList.add('goblin-style');
+    goblinEl.append(goblinContainer);
+}
+
+function renderGoblin() {
+
+}
+
+function displayGoblins() {
+  
+}
+
+
+
 
 // set event listeners 
   // get user input
